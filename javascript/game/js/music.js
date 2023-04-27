c4b3b74499efc3594aa6ed88f1bbf85922aaa628
getGrovy <125417752+getGrovy@users.noreply.github.com>
2023-04-27 21:11:34 +0900

    230427

--- javascript/game/js/music.js
+++ javascript/game/js/music.js
@@ -61,12 +61,17 @@ const musicAudio = muscWrap.querySelector("#main-audio");
 const musicPlay = muscWrap.querySelector("#control-play");
 const musicNextBtn = muscWrap.querySelector("#control-next"); 
 const musicPrevBtn = muscWrap.querySelector("#control-prev"); 
+const musicProgress = muscWrap.querySelector(".progress");
+const musicProgressBar = muscWrap.querySelector(".progress .bar");
+const musicProgressCurrent = muscWrap.querySelector(".timer .current");
+const musicProgressDuration = muscWrap.querySelector(".timer .duration");
 
-// control-play
-let musicIndex =6; //현재 음악 인덱스
+// control-play 
+let musicIndex =1; //현재 음악 인덱스
 
 // 음악재생
 const loadMusic=(num)=>{
+    
     musicName.innerText = allMusic[num-1].name; //뮤직 이름 
     musicArtist.innerText = allMusic[num-1].artist; //뮤직 아티스트
     musicView.src = `img/${allMusic[num-1].img}.png`;//뮤직 이미지
@@ -76,22 +81,65 @@ const loadMusic=(num)=>{
 
 // 재생  
 const playMusic = () =>{
-    alert("play");
+    muscWrap.classList.add("paused");
+    musicPlay.setAttribute("title","정지");
+    musicPlay.setAttribute("class","stop");
+    musicAudio.play();
 }
 // 정지
 const pauseMusic = () =>{
-
+    muscWrap.classList.remove("paused");
+    musicPlay.setAttribute("title","재생");
+    musicPlay.setAttribute("class","play");
+    musicAudio.pause();
 }
 //이전곡 듣기 
 const prevMusic = ()=>{
-    alert("prevMusic");
-
+    
+    musicIndex == 1 ? musicIndex= allMusic.length :musicIndex--;
+    loadMusic(musicIndex);
+    playMusic();
 }
 //다음곡 듣기
 const nextMusic = ()=>{
-    alert("nextMusic");
-    
+    // musicIndex++;
+    musicIndex == allMusic.length ? musicIndex=1 : musicIndex++;
+    loadMusic(musicIndex);
+    playMusic();
 }
+
+//뮤직 진행바
+musicAudio.addEventListener("timeupdate", e=>{
+    // console.log(e);
+    let currentTime = e.target.currentTime; //재생시간
+    let duration = e.target.duration;         //총길이
+    let progressWidth= (currentTime/duration)*100; //시간
+    
+    musicProgressBar.style.width = `${progressWidth}%`;
+    //전체 시간
+    musicAudio.addEventListener("loadeddata", ()=>{
+        let audioDuration = musicAudio.duration;
+        let totalMin = Math.floor(audioDuration/60);
+        let totalSec = Math.floor(audioDuration%60);
+        if(totalSec < 10)totalSec = `0${totalSec}`;
+        musicProgressDuration.innerText =`${totalMin} : ${totalSec}`;
+    })
+    //진행 시간
+    // musicProgressCurrent 
+    let currentMin = Math.floor(currentTime/60) ;
+    let currentSec = Math.floor(currentTime%60) ;
+    if(currentSec < 10)currentSec = `0${currentSec}`;
+    musicProgressCurrent.innerText = `${currentMin} : ${currentSec}`;
+});
+//진행 시간 클릭 
+musicProgress.addEventListener("click", (e)=>{
+    let progressWidth = musicProgress.clientWidth   //진행 전체 길이 
+    let clickOffsetX = e.offsetX ;                  //진행바 기준 측정되는 x 좌표 값
+    let songDuration = musicAudio.duration;         //오디오 전체 길이
+    
+    //백분위 단위로 나눈 숫자를 전체 길이를 곱해서 현재 재생값
+    musicAudio.currentTime = Math.floor((clickOffsetX/progressWidth) * songDuration);
+})
 //이전곡 버튼 클릭
 musicPrevBtn.addEventListener("click",()=>{
     prevMusic();
@@ -103,12 +151,10 @@ musicNextBtn.addEventListener("click",()=>{
 });
 // 플레이 버튼 클릭 
 musicPlay.addEventListener("click",()=>{
-    playMusic();
+    const isMusicPaused = muscWrap.classList.contains("paused");
+    isMusicPaused ? pauseMusic() : playMusic();
+
 });
 window.addEventListener("load",()=>{
     loadMusic(musicIndex);
-    // musicAudio.muted = true;
-    musicAudio.play();
-
-    // musicAudio.muted = false;
 });
