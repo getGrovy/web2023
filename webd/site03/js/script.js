de4df12e6a84e2b26d08e3d01665bb23240a9e05
getGrovy <125417752+getGrovy@users.noreply.github.com>
2023-05-22 01:19:09 +0900

    230522
    gg

---
+++ webd/site03/js/script.js
@@ -0,0 +1,27 @@
+$(function(){
+    let currentIndex = 0;
+    $(".sliderWrap").append($(".slider").first().clone(true));
+    setInterval(function(){
+        currentIndex++;
+        $(".sliderWrap").animate({marginLeft:-100*currentIndex + "%"},600);
+        if(currentIndex == 3){
+            setTimeout(function(){
+                $(".sliderWrap").animate({marginLeft:0},0);
+                currentIndex = 0 ;
+            },700)
+        }
+    },3000);
+
+    $(".nav > ul > li").mouseover(function(){
+        $(this).find(".submenu").stop().slideDown(200);
+    });
+    $(".nav > ul > li").mouseout(function(){
+        $(this).find(".submenu").stop().slideUp(200);
+    });
+    // $(".popup-btn").click(function(){
+    //     $(".popup-view").show();
+    // });
+    // $(".popup-close").click(function(){
+    //     $(".popup-view").hide();
+    // });
+});
